749bca677c25362d3645817d1485bc624b570b12
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-06 11:23:06 -0500

    Set body padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
+			<body className={`antialiased h-screen p-9 sm:p-6`}>{children}</body>
 		</html>
 	);
 }
