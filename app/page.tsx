3babc7dcc5bdd075b28d291915db9ad72ff2c089
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-06 10:50:33 -0500

    Add test FormInput

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/button";
+import { FormInput } from "@/components/form-input";
 import { Typography } from "@/components/typography";
 
 export default function Home() {
@@ -9,6 +10,17 @@ export default function Home() {
 				<Typography variant="body">This is a body text</Typography>
 				<Typography variant="label">This is a label text</Typography>
 				<Button>Click me</Button>
+				<FormInput
+					label="Name"
+					placeholder="Enter your name"
+					id="name"
+					aside={
+						<a href="#" className="text-primary text-sm">
+							Test Aside
+						</a>
+					}
+					error="This is an error"
+				/>
 			</main>
 		</div>
 	);
