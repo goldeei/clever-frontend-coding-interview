316f6953568ee51340737edabd9573b80bbdbc9a
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-06 09:54:27 -0500

    Test text examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Typography } from "@/components/typography";
+
 export default function Home() {
 	return (
 		<div className="">
-			<main className=""></main>
+			<main className="">
+				<Typography variant="h1">Hello World</Typography>
+				<Typography variant="body">This is a body text</Typography>
+				<Typography variant="label">This is a label text</Typography>
+			</main>
 		</div>
 	);
 }
